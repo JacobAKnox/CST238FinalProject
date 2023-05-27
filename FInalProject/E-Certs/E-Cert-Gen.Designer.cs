f463c4db2944aa5bf5345bb8f9a033ec87a728e3
Noah Etchemendy <Noah.etch@gmail.com>
2023-05-27 16:12:25 -0700

    E-Cert Send Added

--- FInalProject/E-Certs/E-Cert-Gen.Designer.cs
+++ FInalProject/E-Certs/E-Cert-Gen.Designer.cs
@@ -31,92 +31,93 @@ namespace FInalProject.E_Certs
         /// </summary>
         private void InitializeComponent()
         {
-            label1 = new Label();
-            checkedListBox1 = new CheckedListBox();
-            textBox1 = new TextBox();
-            label2 = new Label();
-            label3 = new Label();
-            numericUpDown1 = new NumericUpDown();
-            button1 = new Button();
-            ((System.ComponentModel.ISupportInitialize)numericUpDown1).BeginInit();
-            SuspendLayout();
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
             // 
             // label1
             // 
-            label1.AutoSize = true;
-            label1.Location = new Point(58, 12);
-            label1.Name = "label1";
-            label1.Size = new Size(26, 15);
-            label1.TabIndex = 1;
-            label1.Text = "PO:";
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(50, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(25, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "PO:";
             // 
             // checkedListBox1
             // 
-            checkedListBox1.FormattingEnabled = true;
-            checkedListBox1.Location = new Point(90, 12);
-            checkedListBox1.Name = "checkedListBox1";
-            checkedListBox1.Size = new Size(177, 184);
-            checkedListBox1.TabIndex = 2;
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(77, 10);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(152, 154);
+            this.checkedListBox1.TabIndex = 2;
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(90, 202);
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(177, 23);
-            textBox1.TabIndex = 3;
-            textBox1.Text = "name";
+            this.textBox1.Location = new System.Drawing.Point(77, 175);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(152, 20);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = "name";
             // 
             // label2
             // 
-            label2.AutoSize = true;
-            label2.Location = new Point(12, 202);
-            label2.Name = "label2";
-            label2.Size = new Size(72, 15);
-            label2.TabIndex = 4;
-            label2.Text = "Enter Name:";
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 175);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Enter Name:";
             // 
             // label3
             // 
-            label3.AutoSize = true;
-            label3.Location = new Point(30, 231);
-            label3.Name = "label3";
-            label3.Size = new Size(54, 15);
-            label3.TabIndex = 6;
-            label3.Text = "Cert Qty:";
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(26, 200);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Cert Qty:";
             // 
             // numericUpDown1
             // 
-            numericUpDown1.Location = new Point(90, 231);
-            numericUpDown1.Name = "numericUpDown1";
-            numericUpDown1.Size = new Size(64, 23);
-            numericUpDown1.TabIndex = 7;
+            this.numericUpDown1.Location = new System.Drawing.Point(77, 200);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(55, 20);
+            this.numericUpDown1.TabIndex = 7;
             // 
             // button1
             // 
-            button1.Location = new Point(30, 260);
-            button1.Name = "button1";
-            button1.Size = new Size(209, 23);
-            button1.TabIndex = 8;
-            button1.Text = "Generate E-Cert";
-            button1.UseVisualStyleBackColor = true;
+            this.button1.Location = new System.Drawing.Point(26, 225);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(179, 20);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Generate E-Cert";
+            this.button1.UseVisualStyleBackColor = true;
             // 
-            // E-Cert-Gen-Window
+            // E_Cert_Gen
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
-            AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(281, 450);
-            Controls.Add(button1);
-            Controls.Add(numericUpDown1);
-            Controls.Add(label3);
-            Controls.Add(label2);
-            Controls.Add(textBox1);
-            Controls.Add(checkedListBox1);
-            Controls.Add(label1);
-            Name = "E-Cert-Gen-Window";
-            Text = "E-Cert";
-            ((System.ComponentModel.ISupportInitialize)numericUpDown1).EndInit();
-            ResumeLayout(false);
-            PerformLayout();
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(241, 266);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "E_Cert_Gen";
+            this.Text = "E-Cert";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
---
+++ FInalProject/E-Certs/E-Cert-Send.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FInalProject.E_Certs
+{
+    public partial class E_Cert_Send : Form
+    {
+        public E_Cert_Send()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
