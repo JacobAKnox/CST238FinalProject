9ae3a591f6ed8441008df26d1e007ddb914a139d
erobey93 <79769399+erobey93@users.noreply.github.com>
2023-05-28 11:03:43 -0700

    Added in my 2 windows - Update E-Cert Emails and Delete email warning

---
+++ FInalProject/EmailWarning.Designer.cs
@@ -0,0 +1,91 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace FInalProject
+{
+    partial class EmailWarning
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+        private void InitializeComponent()
+        {
+            textBox1 = new TextBox();
+            button1 = new Button();
+            button2 = new Button();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Arial", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox1.Location = new Point(201, 90);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.ReadOnly = true;
+            textBox1.Size = new Size(409, 97);
+            textBox1.TabIndex = 0;
+            textBox1.Text = "\r\nAre you sure that you want to permanently\r\ndelete this email address? ";
+            textBox1.TextAlign = HorizontalAlignment.Center;
+            // 
+            // button1
+            // 
+            button1.BackColor = SystemColors.ActiveCaption;
+            button1.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point);
+            button1.Location = new Point(255, 257);
+            button1.Name = "button1";
+            button1.Size = new Size(131, 55);
+            button1.TabIndex = 1;
+            button1.Text = "Yes";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1Click;
+            // 
+            // button2
+            // 
+            button2.BackColor = SystemColors.ActiveCaption;
+            button2.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point);
+            button2.Location = new Point(431, 257);
+            button2.Name = "button2";
+            button2.Size = new Size(131, 55);
+            button2.TabIndex = 2;
+            button2.Text = "Cancel";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2Click;
+            // 
+            // Form2
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(textBox1);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "Form2";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Warning Window";
+            TopMost = true;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        private TextBox textBox1;
+        private Button button1;
+        private Button button2;
+    }
+}
---
+++ FInalProject/UpdateEmails.Designer.cs
@@ -0,0 +1,293 @@
+﻿using System.Windows.Forms;
+using System;
+using System.Drawing;
+
+namespace FInalProject
+{
+        partial class UpdateEmails: Form
+        {
+
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+            {
+                if (disposing && (components != null))
+                {
+                    components.Dispose();
+                }
+                base.Dispose(disposing);
+            }
+            private void InitializeComponent()
+            {
+                this.components = new System.ComponentModel.Container();
+                this.pictureBox1 = new System.Windows.Forms.PictureBox();
+                this.panel1 = new System.Windows.Forms.Panel();
+                this.button4 = new System.Windows.Forms.Button();
+                this.textBox5 = new System.Windows.Forms.TextBox();
+                this.checkedListBox3 = new System.Windows.Forms.CheckedListBox();
+                this.button3 = new System.Windows.Forms.Button();
+                this.textBox3 = new System.Windows.Forms.TextBox();
+                this.checkedListBox2 = new System.Windows.Forms.CheckedListBox();
+                this.label2 = new System.Windows.Forms.Label();
+                this.label4 = new System.Windows.Forms.Label();
+                this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+                this.textBox2 = new System.Windows.Forms.TextBox();
+                this.button1 = new System.Windows.Forms.Button();
+                this.label1 = new System.Windows.Forms.Label();
+                this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+                this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+                this.textBox1 = new System.Windows.Forms.TextBox();
+                ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+                this.panel1.SuspendLayout();
+                this.SuspendLayout();
+                // 
+                // pictureBox1
+                // 
+                this.pictureBox1.ImageLocation = "C:\\Users\\earob\\OneDrive\\Desktop\\CST238FinalProj\\FinalProjRMSMock\\FinalProjRMSMock" +
+        "\\logo.png";
+                this.pictureBox1.Location = new System.Drawing.Point(25, 28);
+                this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+                this.pictureBox1.Name = "pictureBox1";
+                this.pictureBox1.Size = new System.Drawing.Size(154, 108);
+                this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+                this.pictureBox1.TabIndex = 0;
+                this.pictureBox1.TabStop = false;
+                // 
+                // panel1
+                // 
+                this.panel1.Controls.Add(this.button4);
+                this.panel1.Controls.Add(this.textBox5);
+                this.panel1.Controls.Add(this.checkedListBox3);
+                this.panel1.Controls.Add(this.button3);
+                this.panel1.Controls.Add(this.textBox3);
+                this.panel1.Controls.Add(this.checkedListBox2);
+                this.panel1.Controls.Add(this.label2);
+                this.panel1.Controls.Add(this.label4);
+                this.panel1.Controls.Add(this.checkedListBox1);
+                this.panel1.Controls.Add(this.textBox2);
+                this.panel1.Controls.Add(this.button1);
+                this.panel1.Controls.Add(this.label1);
+                this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+                this.panel1.Location = new System.Drawing.Point(5, 198);
+                this.panel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+                this.panel1.Name = "panel1";
+                this.panel1.Size = new System.Drawing.Size(1309, 520);
+                this.panel1.TabIndex = 2;
+                // 
+                // button4
+                // 
+                this.button4.BackColor = System.Drawing.Color.Black;
+                this.button4.Font = new System.Drawing.Font("Cooper Black", 12F);
+                this.button4.ForeColor = System.Drawing.Color.White;
+                this.button4.Location = new System.Drawing.Point(737, 438);
+                this.button4.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+                this.button4.Name = "button4";
+                this.button4.Size = new System.Drawing.Size(30, 23);
+                this.button4.TabIndex = 23;
+                this.button4.Text = "+";
+                this.button4.UseVisualStyleBackColor = false;
+                this.button4.Click += new System.EventHandler(this.button4Click);
+                // 
+                // textBox5
+                // 
+                this.textBox5.Location = new System.Drawing.Point(555, 440);
+                this.textBox5.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+                this.textBox5.Name = "textBox5";
+                this.textBox5.Size = new System.Drawing.Size(176, 22);
+                this.textBox5.TabIndex = 22;
+                // 
+                // checkedListBox3
+                // 
+                this.checkedListBox3.Items.AddRange(new object[] {
+            "manager1@company.com",
+            "manager2@company.com",
+            "owner@company.com"});
+                this.checkedListBox3.Location = new System.Drawing.Point(555, 360);
+                this.checkedListBox3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+                this.checkedListBox3.Name = "checkedListBox3";
+                this.checkedListBox3.Size = new System.Drawing.Size(212, 72);
+                this.checkedListBox3.TabIndex = 21;
+                // 
+                // button3
+                // 
+                this.button3.BackColor = System.Drawing.Color.Black;
+                this.button3.Font = new System.Drawing.Font("Cooper Black", 12F);
+                this.button3.ForeColor = System.Drawing.Color.White;
+                this.button3.Location = new System.Drawing.Point(737, 294);
+                this.button3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+                this.button3.Name = "button3";
+                this.button3.Size = new System.Drawing.Size(30, 23);
+                this.button3.TabIndex = 20;
+                this.button3.Text = "+";
+                this.button3.UseVisualStyleBackColor = false;
+                this.button3.Click += new System.EventHandler(this.button3Click);
+                // 
+                // textBox3
+                // 
+                this.textBox3.Location = new System.Drawing.Point(555, 295);
+                this.textBox3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+                this.textBox3.Name = "textBox3";
+                this.textBox3.Size = new System.Drawing.Size(176, 22);
+                this.textBox3.TabIndex = 19;
+                // 
+                // checkedListBox2
+                // 
+                this.checkedListBox2.Items.AddRange(new object[] {
+            "ourCompany@company.com",
+            "ourCompanyManager@company.com"});
+                this.checkedListBox2.Location = new System.Drawing.Point(555, 215);
+                this.checkedListBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+                this.checkedListBox2.Name = "checkedListBox2";
+                this.checkedListBox2.Size = new System.Drawing.Size(212, 72);
+                this.checkedListBox2.TabIndex = 18;
+                // 
+                // label2
+                // 
+                this.label2.AutoSize = true;
+                this.label2.Font = new System.Drawing.Font("Arial Black", 12F, System.Drawing.FontStyle.Bold);
+                this.label2.Location = new System.Drawing.Point(459, 360);
+                this.label2.Name = "label2";
+                this.label2.Size = new System.Drawing.Size(44, 28);
+                this.label2.TabIndex = 17;
+                this.label2.Text = "CC";
+                // 
+                // label4
+                // 
+                this.label4.AutoSize = true;
+                this.label4.Font = new System.Drawing.Font("Arial Black", 12F, System.Drawing.FontStyle.Bold);
+                this.label4.Location = new System.Drawing.Point(459, 215);
+                this.label4.Name = "label4";
+                this.label4.Size = new System.Drawing.Size(66, 28);
+                this.label4.TabIndex = 16;
+                this.label4.Text = "From";
+                // 
+                // checkedListBox1
+                // 
+                this.checkedListBox1.Items.AddRange(new object[] {
+            "manager1@company.com",
+            "manager2@company.com"});
+                this.checkedListBox1.Location = new System.Drawing.Point(555, 67);
+                this.checkedListBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+                this.checkedListBox1.Name = "checkedListBox1";
+                this.checkedListBox1.Size = new System.Drawing.Size(212, 72);
+                this.checkedListBox1.TabIndex = 0;
+                this.checkedListBox1.ThreeDCheckBoxes = true;
+                // 
+                // textBox2
+                // 
+                this.textBox2.Location = new System.Drawing.Point(555, 146);
+                this.textBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+                this.textBox2.Name = "textBox2";
+                this.textBox2.Size = new System.Drawing.Size(176, 22);
+                this.textBox2.TabIndex = 10;
+                // 
+                // button1
+                // 
+                this.button1.BackColor = System.Drawing.Color.Black;
+                this.button1.Font = new System.Drawing.Font("Cooper Black", 12F);
+                this.button1.ForeColor = System.Drawing.Color.White;
+                this.button1.Location = new System.Drawing.Point(737, 146);
+                this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+                this.button1.Name = "button1";
+                this.button1.Size = new System.Drawing.Size(30, 23);
+                this.button1.TabIndex = 8;
+                this.button1.Text = "+";
+                this.button1.UseVisualStyleBackColor = false;
+                this.button1.Click += new System.EventHandler(this.button1Click);
+                // 
+                // label1
+                // 
+                this.label1.AutoSize = true;
+                this.label1.Font = new System.Drawing.Font("Arial Black", 12F, System.Drawing.FontStyle.Bold);
+                this.label1.Location = new System.Drawing.Point(480, 67);
+                this.label1.Name = "label1";
+                this.label1.Size = new System.Drawing.Size(38, 28);
+                this.label1.TabIndex = 3;
+                this.label1.Text = "To";
+                // 
+                // imageList1
+                // 
+                this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+                this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
+                this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+                // 
+                // richTextBox2
+                // 
+                this.richTextBox2.BackColor = System.Drawing.SystemColors.Control;
+                this.richTextBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+                this.richTextBox2.Font = new System.Drawing.Font("Arial Black", 19.8F, System.Drawing.FontStyle.Bold);
+                this.richTextBox2.Location = new System.Drawing.Point(490, 51);
+                this.richTextBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+                this.richTextBox2.Name = "richTextBox2";
+                this.richTextBox2.Size = new System.Drawing.Size(400, 42);
+                this.richTextBox2.TabIndex = 4;
+                this.richTextBox2.Text = "Update E-Cert Emails ";
+                // 
+                // textBox1
+                // 
+                this.textBox1.BackColor = System.Drawing.Color.Red;
+                this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+                this.textBox1.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+                this.textBox1.Dock = System.Windows.Forms.DockStyle.Bottom;
+                this.textBox1.Font = new System.Drawing.Font("Arial Black", 16.2F, System.Drawing.FontStyle.Bold);
+                this.textBox1.ForeColor = System.Drawing.Color.White;
+                this.textBox1.Location = new System.Drawing.Point(5, 159);
+                this.textBox1.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+                this.textBox1.Name = "textBox1";
+                this.textBox1.ReadOnly = true;
+                this.textBox1.Size = new System.Drawing.Size(1309, 39);
+                this.textBox1.TabIndex = 12;
+                this.textBox1.Text = "\r\nALL UPDATES \r\nARE PERMANENT\r\n";
+                this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+                // 
+                // EditEmails
+                // 
+                this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+                this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+                this.ClientSize = new System.Drawing.Size(1319, 722);
+                this.Controls.Add(this.textBox1);
+                this.Controls.Add(this.richTextBox2);
+                this.Controls.Add(this.panel1);
+                this.Controls.Add(this.pictureBox1);
+                this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+                this.MaximizeBox = false;
+                this.MinimizeBox = false;
+                this.Name = "EditEmails";
+                this.Padding = new System.Windows.Forms.Padding(5, 4, 5, 4);
+                this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+                this.Text = "Form1";
+                this.TopMost = true;
+                ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+                this.panel1.ResumeLayout(false);
+                this.panel1.PerformLayout();
+                this.ResumeLayout(false);
+                this.PerformLayout();
+
+            }
+
+
+            private PictureBox pictureBox1;
+            private Panel panel1;
+            private TextBox textBox2;
+            private Button button1;
+            private Label label1;
+            private ImageList imageList1;
+            private RichTextBox richTextBox2;
+            private TextBox textBox1;
+            private CheckedListBox checkedListBox3;
+            private Button button3;
+            private TextBox textBox3;
+            private CheckedListBox checkedListBox2;
+            private Label label2;
+            private Label label4;
+            private CheckedListBox checkedListBox1;
+            private Button button4;
+            private TextBox textBox5;
+        }
+}
