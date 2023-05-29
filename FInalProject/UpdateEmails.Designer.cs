18d62eb154294c05c26513642762a6c85b237f7f
erobey93 <79769399+erobey93@users.noreply.github.com>
2023-05-29 11:02:36 -0700

    Fixed issue with checkedListBox event handler being deleted

--- FInalProject/UpdateEmails.Designer.cs
+++ FInalProject/UpdateEmails.Designer.cs
@@ -51,10 +51,10 @@ namespace FinalProject
             // 
             this.pictureBox1.ImageLocation = "C:\\Users\\earob\\OneDrive\\Desktop\\CST238FinalProj\\FinalProjRMSMock\\FinalProjRMSMock" +
     "\\logo.png";
-            this.pictureBox1.Location = new System.Drawing.Point(19, 23);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox1.Location = new System.Drawing.Point(25, 28);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(116, 88);
+            this.pictureBox1.Size = new System.Drawing.Size(155, 108);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
@@ -74,10 +74,10 @@ namespace FinalProject
             this.panel1.Controls.Add(this.button1);
             this.panel1.Controls.Add(this.label1);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.panel1.Location = new System.Drawing.Point(4, 162);
-            this.panel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.panel1.Location = new System.Drawing.Point(5, 199);
+            this.panel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(981, 422);
+            this.panel1.Size = new System.Drawing.Size(1309, 519);
             this.panel1.TabIndex = 2;
             // 
             // button4
@@ -85,10 +85,10 @@ namespace FinalProject
             this.button4.BackColor = System.Drawing.Color.Black;
             this.button4.Font = new System.Drawing.Font("Cooper Black", 12F);
             this.button4.ForeColor = System.Drawing.Color.White;
-            this.button4.Location = new System.Drawing.Point(553, 356);
-            this.button4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button4.Location = new System.Drawing.Point(737, 432);
+            this.button4.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(22, 19);
+            this.button4.Size = new System.Drawing.Size(29, 29);
             this.button4.TabIndex = 23;
             this.button4.Text = "+";
             this.button4.UseVisualStyleBackColor = false;
@@ -96,10 +96,10 @@ namespace FinalProject
             // 
             // textBox5
             // 
-            this.textBox5.Location = new System.Drawing.Point(416, 358);
-            this.textBox5.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox5.Location = new System.Drawing.Point(555, 441);
+            this.textBox5.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(133, 20);
+            this.textBox5.Size = new System.Drawing.Size(176, 22);
             this.textBox5.TabIndex = 22;
             // 
             // checkedListBox3
@@ -108,21 +108,22 @@ namespace FinalProject
             "manager1@company.com",
             "manager2@company.com",
             "owner@company.com"});
-            this.checkedListBox3.Location = new System.Drawing.Point(416, 292);
-            this.checkedListBox3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.checkedListBox3.Location = new System.Drawing.Point(555, 359);
+            this.checkedListBox3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.checkedListBox3.Name = "checkedListBox3";
-            this.checkedListBox3.Size = new System.Drawing.Size(160, 49);
+            this.checkedListBox3.Size = new System.Drawing.Size(212, 55);
             this.checkedListBox3.TabIndex = 21;
+            this.checkedListBox3.ItemCheck += new ItemCheckEventHandler(checkedListBox1_SelectedIndexChanged);
             // 
             // button3
             // 
             this.button3.BackColor = System.Drawing.Color.Black;
             this.button3.Font = new System.Drawing.Font("Cooper Black", 12F);
             this.button3.ForeColor = System.Drawing.Color.White;
-            this.button3.Location = new System.Drawing.Point(553, 239);
-            this.button3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button3.Location = new System.Drawing.Point(737, 289);
+            this.button3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(22, 19);
+            this.button3.Size = new System.Drawing.Size(29, 28);
             this.button3.TabIndex = 20;
             this.button3.Text = "+";
             this.button3.UseVisualStyleBackColor = false;
@@ -130,10 +131,10 @@ namespace FinalProject
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(416, 240);
-            this.textBox3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox3.Location = new System.Drawing.Point(555, 295);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(133, 20);
+            this.textBox3.Size = new System.Drawing.Size(176, 22);
             this.textBox3.TabIndex = 19;
             // 
             // checkedListBox2
@@ -141,20 +142,20 @@ namespace FinalProject
             this.checkedListBox2.Items.AddRange(new object[] {
             "ourCompany@company.com",
             "ourCompanyManager@company.com"});
-            this.checkedListBox2.Location = new System.Drawing.Point(416, 175);
-            this.checkedListBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.checkedListBox2.Location = new System.Drawing.Point(555, 215);
+            this.checkedListBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.checkedListBox2.Name = "checkedListBox2";
-            this.checkedListBox2.Size = new System.Drawing.Size(160, 49);
+            this.checkedListBox2.Size = new System.Drawing.Size(212, 55);
             this.checkedListBox2.TabIndex = 18;
+            this.checkedListBox2.ItemCheck += new ItemCheckEventHandler(checkedListBox1_SelectedIndexChanged);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Arial Black", 12F, System.Drawing.FontStyle.Bold);
-            this.label2.Location = new System.Drawing.Point(344, 292);
-            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Location = new System.Drawing.Point(459, 359);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(34, 23);
+            this.label2.Size = new System.Drawing.Size(44, 28);
             this.label2.TabIndex = 17;
             this.label2.Text = "CC";
             // 
@@ -162,10 +163,9 @@ namespace FinalProject
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Arial Black", 12F, System.Drawing.FontStyle.Bold);
-            this.label4.Location = new System.Drawing.Point(344, 175);
-            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Location = new System.Drawing.Point(459, 215);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(54, 23);
+            this.label4.Size = new System.Drawing.Size(66, 28);
             this.label4.TabIndex = 16;
             this.label4.Text = "From";
             // 
@@ -174,19 +174,20 @@ namespace FinalProject
             this.checkedListBox1.Items.AddRange(new object[] {
             "manager1@company.com",
             "manager2@company.com"});
-            this.checkedListBox1.Location = new System.Drawing.Point(416, 54);
-            this.checkedListBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.checkedListBox1.Location = new System.Drawing.Point(555, 66);
+            this.checkedListBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.checkedListBox1.Name = "checkedListBox1";
-            this.checkedListBox1.Size = new System.Drawing.Size(160, 49);
+            this.checkedListBox1.Size = new System.Drawing.Size(212, 55);
             this.checkedListBox1.TabIndex = 0;
             this.checkedListBox1.ThreeDCheckBoxes = true;
+            this.checkedListBox1.ItemCheck += new ItemCheckEventHandler(checkedListBox1_SelectedIndexChanged); 
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(416, 119);
-            this.textBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox2.Location = new System.Drawing.Point(555, 146);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(133, 20);
+            this.textBox2.Size = new System.Drawing.Size(176, 22);
             this.textBox2.TabIndex = 10;
             // 
             // button1
@@ -194,10 +195,10 @@ namespace FinalProject
             this.button1.BackColor = System.Drawing.Color.Black;
             this.button1.Font = new System.Drawing.Font("Cooper Black", 12F);
             this.button1.ForeColor = System.Drawing.Color.White;
-            this.button1.Location = new System.Drawing.Point(553, 119);
-            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Location = new System.Drawing.Point(737, 141);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(22, 19);
+            this.button1.Size = new System.Drawing.Size(29, 28);
             this.button1.TabIndex = 8;
             this.button1.Text = "+";
             this.button1.UseVisualStyleBackColor = false;
@@ -207,10 +208,9 @@ namespace FinalProject
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Arial Black", 12F, System.Drawing.FontStyle.Bold);
-            this.label1.Location = new System.Drawing.Point(360, 54);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(480, 66);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(32, 23);
+            this.label1.Size = new System.Drawing.Size(38, 28);
             this.label1.TabIndex = 3;
             this.label1.Text = "To";
             // 
@@ -225,10 +225,10 @@ namespace FinalProject
             this.richTextBox2.BackColor = System.Drawing.SystemColors.Control;
             this.richTextBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.richTextBox2.Font = new System.Drawing.Font("Arial Black", 19.8F, System.Drawing.FontStyle.Bold);
-            this.richTextBox2.Location = new System.Drawing.Point(368, 41);
-            this.richTextBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.richTextBox2.Location = new System.Drawing.Point(491, 50);
+            this.richTextBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.richTextBox2.Name = "richTextBox2";
-            this.richTextBox2.Size = new System.Drawing.Size(300, 34);
+            this.richTextBox2.Size = new System.Drawing.Size(400, 42);
             this.richTextBox2.TabIndex = 4;
             this.richTextBox2.Text = "Update E-Cert Emails ";
             // 
@@ -240,29 +240,29 @@ namespace FinalProject
             this.textBox1.Dock = System.Windows.Forms.DockStyle.Bottom;
             this.textBox1.Font = new System.Drawing.Font("Arial Black", 16.2F, System.Drawing.FontStyle.Bold);
             this.textBox1.ForeColor = System.Drawing.Color.White;
-            this.textBox1.Location = new System.Drawing.Point(4, 131);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.textBox1.Location = new System.Drawing.Point(5, 160);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
             this.textBox1.Name = "textBox1";
             this.textBox1.ReadOnly = true;
-            this.textBox1.Size = new System.Drawing.Size(981, 31);
+            this.textBox1.Size = new System.Drawing.Size(1309, 39);
             this.textBox1.TabIndex = 12;
             this.textBox1.Text = "\r\nALL UPDATES \r\nARE PERMANENT\r\n";
             this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // UpdateEmails
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(989, 587);
+            this.ClientSize = new System.Drawing.Size(1319, 722);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.richTextBox2);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.pictureBox1);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "UpdateEmails";
-            this.Padding = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Padding = new System.Windows.Forms.Padding(5, 4, 5, 4);
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
             this.TopMost = true;
