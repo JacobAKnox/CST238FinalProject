3a43e32f8024c256239a28c93d9620e9b95e8bfe
Noah Etchemendy <Noah.etch@gmail.com>
2023-06-05 11:20:36 -0700

    Send E_Cert now has proper window title

--- FInalProject/E-Certs/E-Cert-Send.Designer.cs
+++ FInalProject/E-Certs/E-Cert-Send.Designer.cs
@@ -214,7 +214,7 @@
             this.ClientSize = new System.Drawing.Size(666, 645);
             this.Controls.Add(this.tableLayoutPanel2);
             this.Name = "E_Cert_Send";
-            this.Text = "E_Cert_Send";
+            this.Text = "Send E-Cert";
             this.Load += new System.EventHandler(this.E_Cert_Send_Load);
             this.tableLayoutPanel2.ResumeLayout(false);
             this.tableLayoutPanel2.PerformLayout();
